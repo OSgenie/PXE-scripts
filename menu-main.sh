68177015cfe56f9c0e76125cae4771740aae54ff
kirtley <kirtley@kirtley-zbox>
2013-01-29 01:11:32 -0800

    .

--- menu-main.sh
+++ menu-main.sh
@@ -16,22 +16,22 @@ fi
 function create_menu_header ()
 {
 cat > $menupath <'EOM'
-MENU TITLE --== Main Menu ==--
-DEFAULT vesamenu.c32
-TIMEOUT 200 #this is optional - will start the default after 20 seconds
-ONTIMEOUT BootLocal
-PROMPT 0
-#
-LABEL kirtley-workstation
-MENU DEFAULT
-MENU LABEL Kirtley Workstation
-KERNEL pxe-kw/vmlinuz-3.2.0-29-generic-pae
-APPEND root=/dev/nfs initrd=pxe-kw/initrd.img-3.2.0-29-generic-pae nfsroot=192.168.11.88:/var/nfs/pxe-kw,rw ip=dhcp rw
-#this allows you to exit the pxe stack and pass booting to the local system
-LABEL BootLocal
-MENU LABEL Local Boot
-localboot 0
-#
+"MENU TITLE --== Main Menu ==--"
+"DEFAULT vesamenu.c32"
+"TIMEOUT 200 #this is optional - will start the default after 20 seconds"
+"ONTIMEOUT BootLocal"
+"PROMPT 0"
+"#"
+"LABEL kirtley-workstation"
+"MENU DEFAULT"
+"MENU LABEL Kirtley Workstation"
+"KERNEL pxe-kw/vmlinuz-3.2.0-29-generic-pae"
+"APPEND root=/dev/nfs initrd=pxe-kw/initrd.img-3.2.0-29-generic-pae nfsroot=192.168.11.88:/var/nfs/pxe-kw,rw ip=dhcp rw"
+"#this allows you to exit the pxe stack and pass booting to the local system"
+"LABEL BootLocal"
+"MENU LABEL Local Boot"
+"localboot 0"
+"#"
 EOM
 }
 

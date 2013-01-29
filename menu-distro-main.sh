160c4b6d87cc6f28e011ae2f7476339f629fc8b7
kirtley <kirtley@kirtley-zbox>
2013-01-29 01:39:52 -0800

    .

--- menu-distro-main.sh
+++ menu-distro-main.sh
@@ -16,28 +16,28 @@ function create_distro_menus ()
 {
 menusarray=$( ls $tftpfolder/menus )
 for folder in $menusarray; do
-echo $folder
-menupath="$tftpfolder/menus/$folder.conf"
-cat > $menupath <<'EOM'
-MENU TITLE --== $folder Menu ==--
-#
-LABEL rootmenu
-MENU LABEL <---- Main Menu
-kernel vesamenu.c32
-append mainmenu.conf
-#
+	echo $folder
+	menupath="$tftpfolder/menus/$folder.conf"
+	cat > $menupath <<'EOM'
+	MENU TITLE --== $folder Menu ==--
+	#
+	LABEL rootmenu
+	   MENU LABEL <---- Main Menu
+	   kernel vesamenu.c32
+		append mainmenu.conf
+	#
 EOM
-for conf in $tftpfolder/menus/$folder/*; do 
-fullname=$(basename $conf)
-extension=${fullname##*.}
-name=$(basename $conf .$extension)
-if [ $extension = conf ]; then
-echo $fullname
-cat >> $menupath <<'EOM'
-LABEL $name
-MENU LABEL $name --->
-kernel vesamenu.c32
-append /menus/$folder/$fullname
+	for conf in $tftpfolder/menus/$folder/*; do 
+		fullname=$(basename $conf)
+		extension=${fullname##*.}
+		name=$(basename $conf .$extension)
+		if [ $extension == conf ]; then
+			echo $fullname
+			cat >> $menupath <<EOM
+			LABEL $name
+				MENU LABEL $name --->
+			   kernel vesamenu.c32
+			   append /menus/$folder/$fullname
 EOM		
 		fi
 	done

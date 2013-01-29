1f737e190596e97e19bbcfa712b637fa45a3f975
kirtley <kirtley@kirtley-zbox>
2013-01-29 02:22:54 -0800

    .

--- menu-distro-install.sh
+++ menu-distro-install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # Kirtley Wienbroer
 # kirtley@osgenie.com
-# June 24 2012
 # Generate PXE boot isodistro from extracted isos
 
 function check_for_sudo ()
@@ -12,8 +11,6 @@ if [ $UID != 0 ]; then
 fi
 }
 
-check_for_sudo
-
 nfshost=192.168.11.88
 nfspath=$nfshost:/pxeboot
 nfsrootpath=$nfshost:/var/nfs/pxeboot
@@ -22,33 +19,31 @@ seedpath=http://192.168.11.10/preseed
 seedfile="ubuntu.seed"
 
 mount -t nfs4 $nfspath /mnt/pxeboot
-for folder in /mnt/pxeboot/*
-do
+for folder in /mnt/pxeboot/*; do
 distro=$(basename "$folder")
 menupath="$tftpfolder/menus/install/$distro.conf"
 # create distro PXE boot menu
 echo "creating $distro menu..."
-echo "MENU TITLE --== $distro ==-- " > $menupath
-echo " " >> $menupath
-echo "LABEL rootmenu" >> $menupath
-echo "        MENU LABEL <---- Install Menu" >> $menupath
-echo "        kernel ubuntu-installer/i386/boot-screens/vesamenu.c32" >> $menupath
-echo "        append menus/install.conf" >> $menupath
-echo " " >> $menupath
+cat > $menupath << EOM
+MENU TITLE --== $distro ==-- 
+ 
+LABEL rootmenu
+        MENU LABEL <---- Install Menu
+        kernel ubuntu-installer/i386/boot-screens/vesamenu.c32
+        append menus/install.conf
 
-
+EOM
 	# PXE boot menu entry for each iso
 	revisionarray=$( ls -r $folder )
 	for revision in $revisionarray
 	do
-	if [ ! "$revision" = "stock" ];then
+	if [ ! "$revision" = "stock" ]; then
 	revdate=$(date --rfc-3339=seconds -d @$revision)
 	else
 	revdate=$revision
 	fi
 	subfolderarray=$folder/$revision
-		for subfolder in $subfolderarray
-		do
+		for subfolder in $subfolderarray; do
 		revision=$(basename "$subfolder")
 		bootfolder=isodistro/$distro/$revision
 		if [ -e "$subfolder/casper/initrd.lz" ]; then
@@ -57,22 +52,25 @@ echo " " >> $menupath
 		  	mkdir -p $tftpfolder/$kernelpath
 			cp -uv $subfolder/casper/vmlinuz $tftpfolder/$kernelpath/
 			cp -uv $subfolder/casper/initrd.lz $tftpfolder/$kernelpath/
-		  echo "LABEL $revdate" >> $menupath
-		  echo "	MENU LABEL $revdate" >> $menupath
-		  echo "	kernel $kernelpath/vmlinuz" >> $menupath
-		  echo "	append initrd=$kernelpath/initrd.lz noprompt boot=casper only-ubiquity url=$seedpath/$seedfile oem-config/enable=true netboot=nfs nfsroot=$nfsrootpath/$distro/$revision ro toram -" >> $menupath
-		  echo " " >> $menupath
+			cat >> $menupath << EOM
+LABEL $revdate
+MENU LABEL $revdate
+    kernel $kernelpath/vmlinuz
+    append initrd=$kernelpath/initrd.lz noprompt boot=casper only-ubiquity url=$seedpath/$seedfile oem-config/enable=true netboot=nfs nfsroot=$nfsrootpath/$distro/$revision ro toram -
+EOM
 		elif [ -e "$subfolder/casper/initrd.gz" ]; then
 			kernelpath=$bootfolder/casper
 		  	echo "$revision - casper!"
 		  	mkdir -p $tftpfolder/$kernelpath
 			cp -uv $subfolder/casper/vmlinuz $tftpfolder/$kernelpath/
 			cp -uv $subfolder/casper/initrd.gz $tftpfolder/$kernelpath/
-		  echo "LABEL $revdate" >> $menupath
-		  echo "	MENU LABEL $revdate" >> $menupath
-		  echo "	kernel $kernelpath/vmlinuz" >> $menupath
-		  echo "	append initrd=$kernelpath/initrd.gz noprompt boot=casper only-ubiquity url=$seedpath/$seedfile oem-config/enable=true netboot=nfs nfsroot=$nfsrootpath/$distro/$revision ro toram -" >> $menupath
-		  echo " " >> $menupath
+			cat >> $menupath << EOM
+LABEL $revdate
+MENU LABEL $revdate
+    kernel $kernelpath/vmlinuz
+    append initrd=$kernelpath/initrd.gz noprompt boot=casper only-ubiquity url=$seedpath/$seedfile oem-config/enable=true netboot=nfs nfsroot=$nfsrootpath/$distro/$revision ro toram -
+
+EOM
 		elif [ -e "$subfolder/install" ]; then
 		  if [[ $distro == *amd64* ]]; then
 		  cpu=amd64
@@ -84,12 +82,13 @@ echo " " >> $menupath
 		  mkdir -p $tftpfolder/$kernelpath
 		  cp -uv $subfolder/install/netboot/ubuntu-installer/$cpu/linux $tftpfolder/$kernelpath/
 		  cp -uv $subfolder/install/netboot/ubuntu-installer/$cpu/initrd.gz $tftpfolder/$kernelpath/
-		  
-		  echo "LABEL $revdate" >> $menupath
-		  echo "	MENU LABEL $revdate" >> $menupath
-		  echo "	kernel $kernelpath/linux" >> $menupath
-		  echo "	append initrd=$kernelpath/initrd.gz noprompt netboot=nfs url=$seedpath/$seedfile root=/dev/nfs nfsroot=$nfspath/$distro/$revision/ ip=dhcp rw" >> $menupath
-		  echo " " >> $menupath
+		  cat >> $menupath << EOM
+LABEL $revdate
+MENU LABEL $revdate
+    kernel $kernelpath/linux
+    append initrd=$kernelpath/initrd.gz noprompt netboot=nfs url=$seedpath/$seedfile root=/dev/nfs nfsroot=$nfspath/$distro/$revision/ ip=dhcp rw
+
+EOM
 		else 
 		  echo "ERROR - $distro-$revision"
 		fi
@@ -97,5 +96,3 @@ echo " " >> $menupath
 	done
 done
 umount /mnt/pxeboot
-#/mnt/share/ubuntu/ubuntu-10.04.2-server-amd64/install/netboot/ubuntu-installer/amd64/linux
-#noprompt cdrom-detect/try-usb=true file=/cdrom/preseed/auto.seed boot=casper only-ubiquity initrd=/casper/initrd.lz oem-config/enable=true automatic-ubiquity splash quiet toram
--- menu-distro-live.sh
+++ menu-distro-live.sh
@@ -22,63 +22,60 @@ seedpath=http://192.168.11.10/preseed
 seedfile="ubuntu.seed"
 
 mount -t nfs4 $nfspath /mnt/pxeboot
-for folder in /mnt/pxeboot/*
-do
+for folder in /mnt/pxeboot/*; do
 distro=$(basename "$folder")
 menupath="$tftpfolder/menus/live/$distro.conf"
 # create distro PXE boot menu
 echo "creating $distro menu..."
-echo "MENU TITLE --== $distro ==-- " > $menupath
-echo " " >> $menupath
-echo "LABEL rootmenu" >> $menupath
-echo "        MENU LABEL <---- Live Menu" >> $menupath
-echo "        kernel vesamenu.c32" >> $menupath
-echo "        append menus/live.conf" >> $menupath
-echo " " >> $menupath
-
-	# PXE boot menu entry for each iso
-	revisionarray=$( ls -r $folder )
-	for revision in $revisionarray
-	do
-	if [ ! "$revision" = "stock" ];then
-	revdate=$(date --rfc-3339=seconds -d @$revision)
-	else
-	revdate=$revision
-	fi
-	subfolderarray=$folder/$revision
-		for subfolder in $subfolderarray
-		do
-		revision=$(basename "$subfolder")
-		bootfolder=isodistro/$distro/$revision
-		if [ -e "$subfolder/casper/initrd.lz" ]; then
-			kernelpath=$bootfolder/casper
-		  	echo "$revision - casper!"
-		  	mkdir -p $tftpfolder/$kernelpath
-			cp -uv $subfolder/casper/vmlinuz $tftpfolder/$kernelpath/
-			cp -uv $subfolder/casper/initrd.lz $tftpfolder/$kernelpath/
-		  echo "LABEL "$revdate >> $menupath
-		  echo "	MENU LABEL "$revdate >> $menupath
-		  echo "	kernel $kernelpath/vmlinuz" >> $menupath
-		  echo "	append initrd=$kernelpath/initrd.lz noprompt boot=casper url=$seedpath/$seedfile netboot=nfs nfsroot=$nfsrootpath/$distro/$revision ro toram -" >> $menupath
-		  echo " " >> $menupath
-		elif [ -e "$subfolder/casper/initrd.gz" ]; then
-			kernelpath=$bootfolder/casper
-		  	echo "$revision - casper!"
-		  	mkdir -p $tftpfolder/$kernelpath
-			cp -uv $subfolder/casper/vmlinuz $tftpfolder/$kernelpath/
-			cp -uv $subfolder/casper/initrd.gz $tftpfolder/$kernelpath/
-		  echo "LABEL "$revdate >> $menupath
-		  echo "	MENU LABEL "$revdate >> $menupath
-		  echo "	kernel $kernelpath/vmlinuz" >> $menupath
-		  echo "	append initrd=$kernelpath/initrd.gz noprompt boot=casper url=$seedpath/$seedfile netboot=nfs nfsroot=$nfsrootpath/$distro/$revision ro toram -" >> $menupath
-		  echo " " >> $menupath	
-		else 
-		echo " not a live cd"
-		rm $menupath  
-		fi
-		done
-	done
+cat > $menupath << EOM
+MENU TITLE --== $distro ==--
+#
+LABEL rootmenu
+MENU LABEL <---- Live Menu
+kernel vesamenu.c32
+append menus/live.conf
+EOM
+# PXE boot menu entry for each iso
+revisionarray=$( ls -r $folder )
+for revision in $revisionarray; do
+    if [ ! "$revision" = "stock" ]; then
+        revdate=$(date --rfc-3339=seconds -d @$revision)
+    else
+        revdate=$revision
+    fi
+    subfolderarray=$folder/$revision
+    for subfolder in $subfolderarray; do
+        revision=$(basename "$subfolder")
+        bootfolder=isodistro/$distro/$revision
+        if [ -e "$subfolder/casper/initrd.lz" ]; then
+            kernelpath=$bootfolder/casper
+            echo "$revision - casper!"
+            mkdir -p $tftpfolder/$kernelpath
+            cp -uv $subfolder/casper/vmlinuz $tftpfolder/$kernelpath/
+            cp -uv $subfolder/casper/initrd.lz $tftpfolder/$kernelpath/
+            cat >> $menupath < EOM
+LABEL $revdate >> $menupath
+MENU LABEL $revdate >> $menupath
+    kernel $kernelpath/vmlinuz
+    append initrd=$kernelpath/initrd.lz noprompt boot=casper url=$seedpath/$seedfile netboot=nfs nfsroot=$nfsrootpath/$distro/$revision ro toram -
+EOM
+        elif [ -e "$subfolder/casper/initrd.gz" ]; then
+            kernelpath=$bootfolder/casper
+            echo "$revision - casper!"
+            mkdir -p $tftpfolder/$kernelpath
+            cp -uv $subfolder/casper/vmlinuz $tftpfolder/$kernelpath/
+            cp -uv $subfolder/casper/initrd.gz $tftpfolder/$kernelpath/
+            cat >> $menupath << EOM
+LABEL $revdate >> $menupath
+MENU LABEL $revdate >> $menupath
+    kernel $kernelpath/vmlinuz
+    append initrd=$kernelpath/initrd.gz noprompt boot=casper url=$seedpath/$seedfile netboot=nfs nfsroot=$nfsrootpath/$distro/$revision ro toram -
+EOM
+        else 
+            echo " not a live cd"
+        rm $menupath  
+        fi
+    done
+done
 done
 umount /mnt/pxeboot
-#/mnt/share/ubuntu/ubuntu-10.04.2-server-amd64/install/netboot/ubuntu-installer/amd64/linux
-#noprompt cdrom-detect/try-usb=true file=/cdrom/preseed/auto.seed boot=casper only-ubiquity initrd=/casper/initrd.lz oem-config/enable=true automatic-ubiquity splash quiet toram

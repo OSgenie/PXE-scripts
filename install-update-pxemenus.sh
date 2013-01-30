1d42c94539b514448fb99fcf04ba4c0797511861
kirtley <kirtley@kirtley-zbox>
2013-01-29 23:17:45 -0800

    .

--- install-update-pxemenus.sh
+++ install-update-pxemenus.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-sudo install menu-update-all.sh /usr/local/bin/update-pxemenus
+sudo install menu-update-all.sh /usr/local/bin/build-pxemenus
 sudo install menu-distro-install.sh /usr/local/bin/menu-distro-install.sh
 sudo install menu-distro-live.sh /usr/local/bin/menu-distro-live.sh
 sudo install menu-distro-main.sh /usr/local/bin/menu-distro-main.sh
 sudo install menu-distro-stock.sh /usr/local/bin/menu-distro-stock.sh
 sudo install menu-main.sh /usr/local/bin/menu-main.sh
 #menu-utilities.sh
+
+sudo crontab -l | { cat; echo "5-55/10 * * * * /usr/local/bin/build-pxemenus"; } | sudo crontab -

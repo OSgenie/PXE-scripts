cbc0ba1190c1d9940c7469e4f532dda89dfa3aac
Live session user <lubuntu@lubuntu.(none)>
2013-01-30 02:32:43 +0000

    .

---
+++ install-update-pxemenus.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo install menu-update-all.sh /usr/local/bin/update-pxemenus
+sudo install menu-distro-install.sh /usr/local/bin/menu-distro-install.sh
+sudo install menu-distro-live.sh /usr/local/bin/menu-distro-live.sh
+sudo install menu-distro-main.sh /usr/local/bin/menu-distro-main.sh
+sudo install menu-distro-stock.sh /usr/local/bin/menu-distro-stock.sh
+sudo install menu-main.sh /usr/local/bin/menu-main.sh
+#menu-utilities.sh

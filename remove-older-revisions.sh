63190a0279f4971ef8c7536208b53cf90303afac
kirtley <kirtley@kirtley-zbox>
2013-02-02 06:52:46 -0800

    .

---
+++ remove-older-revisions.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+directories=(
+/var/nfs/updatediso/install
+/var/nfs/updatediso/live
+/var/nfs/pxeboot/install
+/var/nfs/pxeboot/live
+/var/lib/tftpboot/boot/
+)
+
+function check_for_sudo ()
+{
+if [ $UID != 0 ]; then
+        echo "You need root privileges"
+        exit 2
+fi
+}
+
+function delete_older_isos ()
+{
+find /var/lib/tftpboot/boot/*/gold -exec touch {} +
+for dir in $directories; do
+    find $dir/* -mtime +28 -delete
+done
+}
+
+check_for_sudo
+delete_older_isos

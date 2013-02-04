088a1fdfdf8be5476826007d9e347daf349d8721
kirtley <kirtley@kirtley-zbox>
2013-02-04 10:43:43 -0800

    .

--- generate-update-lists.sh
+++ generate-update-lists.sh
@@ -62,6 +62,7 @@ for iso in $all_isos; do
     echo $iso
     iso_already_in_list=$(grep "$iso" $folderpath/updatelists/*)
     if [ "$iso_already_in_list" == "" ]; then
+        clear
         echo "Which distrubution of Ubuntu is $iso based on?"
         add_iso_to_list
         echo $iso_name | tee -a $folderpath/updatelists/$selected_list

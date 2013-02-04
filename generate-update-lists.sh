81cb523eb27880cb5d1be0c742d93d69d1d70f04
kirtley <kirtley@kirtley-zbox>
2013-02-04 10:24:46 -0800

    .

--- generate-update-lists.sh
+++ generate-update-lists.sh
@@ -39,7 +39,7 @@ for server_release in $supported_releases; do
 #        echo ${list[@]}
 #        for (( i=0;i<${#list[@]};i++)); do
 #            echo ${list[$i]} | tee -a $folderpath/updatelists/$updatelist
-        done
+#        done
     done
 done
 }

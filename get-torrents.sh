9a17873bf72e4648800395d24c8092f1b059b5ef
kirtley <kirtley@wienbroer.com>
2019-01-02 20:53:22 -0500

    modified:   get-torrents.sh 	new file:   torrent.configs/linuxmint/linuxmint.arches 	renamed:    torrent.configs/linuxmint.config -> torrent.configs/linuxmint/linuxmint.config 	new file:   torrent.configs/linuxmint/linuxmint.releases 	renamed:    torrent.configs/kubuntu.config -> torrent.configs/ubuntu/kubuntu.config 	renamed:    torrent.configs/lubuntu.config -> torrent.configs/ubuntu/lubuntu.config 	renamed:    torrent.configs/ubuntu-budgie.config -> torrent.configs/ubuntu/ubuntu-budgie.config 	renamed:    torrent.configs/ubuntu-mate.config -> torrent.configs/ubuntu/ubuntu-mate.config 	new file:   torrent.configs/ubuntu/ubuntu.arches 	renamed:    torrent.configs/ubuntu.config -> torrent.configs/ubuntu/ubuntu.config 	new file:   torrent.configs/ubuntu/ubuntu.releases 	renamed:    torrent.configs/ubuntustudio.config -> torrent.configs/ubuntu/ubuntustudio.config 	renamed:    torrent.configs/xubuntu.config -> torrent.configs/ubuntu/xubuntu.config

--- get-torrents.sh
+++ get-torrents.sh
@@ -1,28 +1,32 @@
 #!/bin/bash
-script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-distros=torrent.configs/*
+script_dir=/usr/local/bin/torrent.configs
+distros_base=torrent.configs/*
 download_folder=/var/nfs/transmission/torrents/
 
-for distro in $distros; do
-  distro=$(basename -s .config $distro)
-  source torrent.configs/$distro".config"
-  for arch in $arches; do
-    for flavor in $flavors; do
-      for release in $releases; do
-        source $script_dir/$distro".config"
-        for i in {10..0}; do
-          cd $download_folder
-          if [ $i == 0 ];then
-            echo "checking "$release_server"/"$release_folder"/"$distro"-"$release"-"$flavor"-"$arch".iso.torrent"
-            /usr/bin/wget --no-clobber -t 1 $release_server"/"$release_folder"/"$distro"-"$release"-"$flavor"-"$arch".iso.torrent"
-          else
-            echo "checking "$release_server"/"$release_folder"/"$distro"-"$release"."$i"-"$flavor"-"$arch".iso.torrent"
-            /usr/bin/wget --no-clobber -t 1 $release_server"/"$release_folder"/"$distro"-"$release"."$i"-"$flavor"-"$arch".iso.torrent"
-          fi
-          if [ -f $distro"-"$release"."$i"-"$flavor"-"$arch".iso.torrent" ] || [ -f  $distro"-"$release"-"$flavor"-"$arch".iso.torrent" ] \
-          || [ -f $distro"-"$release"."$i"-"$flavor"-"$arch".iso.torrent.added" ] || [ -f  $distro"-"$release"-"$flavor"-"$arch".iso.torrent.added" ];then
-            break
-          fi
+for distros in $distros_base; do
+  source $script_dir/$distros/$distros".arches"
+  for distro in $distros; do
+    distro=$(basename -s .config $distro)
+    source $script_dir/$distros/$distro".config"
+    for arch in $arches; do
+      for flavor in $flavors; do
+        source $script_dir/$distros/$distros".releases"
+        for release in $releases; do
+          for i in {10..0}; do
+            source $script_dir/$distros/$distro".config"
+            cd $download_folder
+            if [ $i == 0 ];then
+              echo "checking "$release_server"/"$release_folder"/"$distro"-"$release"-"$flavor"-"$arch".iso.torrent"
+              /usr/bin/wget --no-clobber -t 1 $release_server"/"$release_folder"/"$distro"-"$release"-"$flavor"-"$arch".iso.torrent"
+            else
+              echo "checking "$release_server"/"$release_folder"/"$distro"-"$release"."$i"-"$flavor"-"$arch".iso.torrent"
+              /usr/bin/wget --no-clobber -t 1 $release_server"/"$release_folder"/"$distro"-"$release"."$i"-"$flavor"-"$arch".iso.torrent"
+            fi
+            if [ -f $distro"-"$release"."$i"-"$flavor"-"$arch".iso.torrent" ] || [ -f  $distro"-"$release"-"$flavor"-"$arch".iso.torrent" ] \
+            || [ -f $distro"-"$release"."$i"-"$flavor"-"$arch".iso.torrent.added" ] || [ -f  $distro"-"$release"-"$flavor"-"$arch".iso.torrent.added" ];then
+              break
+            fi
+          done
         done
       done
     done

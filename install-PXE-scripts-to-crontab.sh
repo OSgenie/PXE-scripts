1bad1c2981d46b5725ed9625a4139bd1b87d7fdf
kirtley <kirtley@wienbroer.com>
2019-01-03 17:23:42 -0500

    modified:   install-PXE-scripts-to-crontab.sh

--- install-PXE-scripts-to-crontab.sh
+++ install-PXE-scripts-to-crontab.sh
@@ -4,6 +4,7 @@ http_preseed_root=/var/nfs/pxeboot/preseed
 binary_dir=/usr/local/bin
 application_dir=$binary_dir/iso2pxe
 
+
 function check_for_sudo ()
 {
 	if [ $UID != 0 ]; then
@@ -18,33 +19,33 @@ function install_config_source_local_bin ()
 	echo "tftp_folder=/var/lib/tftpboot" | tee -a $script_dir/pxe.config
 	echo "nfs_server=\$server_ip" | tee -a $script_dir/pxe.config
 	echo "seed_path=http://\$server_ip/preseed" | tee -a $script_dir/pxe.config
-	install $script_dir/pxe.config $application_directory/pxe.config
+	install $script_dir/pxe.config $application_dir/pxe.config
 }
 function install_scripts_local_bin ()
 {
-	install -d $application_directory
-	install $script_dir/build-pxemenus.sh $application_directory/build-pxemenus
-	install $script_dir/create-install-menus.sh $application_directory/create-install-menus
-	install $script_dir/create-live-menus.sh $application_directory/create-live-menus
-	install $script_dir/create-submenus.sh $application_directory/create-submenus
-	install $script_dir/create-stock-menus.sh $application_directory/create-stock-menus
-	install $script_dir/create-main-menu.sh $application_directory/create-main-menu
-	install $script_dir/create-server-alternate-menus.sh $application_directory/create-server-alternate-menus
-	#install $script_dir/create-utility-menu.sh $application_directory/create-utility-menu
-	install $script_dir/nfs-extract-iso.sh $application_directory/extract-isos
-	install $script_dir/remove-older-iso-revisions.sh $application_directory/remove-older-iso-revisions
-	install $script_dir/generate-update-lists.sh $application_directory/generate-update-lists
-	install $script_dir/get-torrents.sh $application_directory/get-torrents
-	cp -r $script_dir/torrent.configs $application_directory/
+	install -d $application_dir
+	install $script_dir/build-pxemenus.sh $application_dir/build-pxemenus
+	install $script_dir/create-install-menus.sh $application_dir/create-install-menus
+	install $script_dir/create-live-menus.sh $application_dir/create-live-menus
+	install $script_dir/create-submenus.sh $application_dir/create-submenus
+	install $script_dir/create-stock-menus.sh $application_dir/create-stock-menus
+	install $script_dir/create-main-menu.sh $application_dir/create-main-menu
+	install $script_dir/create-server-alternate-menus.sh $application_dir/create-server-alternate-menus
+	#install $script_dir/create-utility-menu.sh $application_dir/create-utility-menu
+	install $script_dir/nfs-extract-iso.sh $application_dir/extract-isos
+	install $script_dir/remove-older-iso-revisions.sh $application_dir/remove-older-iso-revisions
+	install $script_dir/generate-update-lists.sh $application_dir/generate-update-lists
+	install $script_dir/get-torrents.sh $application_dir/get-torrents
+	cp -r $script_dir/torrent.configs $application_dir/
 }
 
 function configure_crontab ()
 {
 	echo "# m h  dom mon dow   command" | crontab -
-	crontab -l | { cat; echo "*/10 * * * * $application_directory/build-pxemenus  > /var/log/build-pxemenus.log"; } | crontab -
-	crontab -l | { cat; echo "2-52/10 * * * * $application_directory/extract-isos  > /var/log/extract-isos.log"; } | crontab -
-	crontab -l | { cat; echo "@weekly $application_directory/remove-older-iso-revisions  > /var/log/remove-older-isos.log"; } | crontab -
-	crontab -l | { cat; echo "@weekly $application_directory/get-torrents  > /var/log/get-torrents.log"; } | crontab -
+	crontab -l | { cat; echo "*/10 * * * * $application_dir/build-pxemenus  > /var/log/build-pxemenus.log"; } | crontab -
+	crontab -l | { cat; echo "2-52/10 * * * * $application_dir/extract-isos  > /var/log/extract-isos.log"; } | crontab -
+	crontab -l | { cat; echo "@weekly $application_dir/remove-older-iso-revisions  > /var/log/remove-older-isos.log"; } | crontab -
+	crontab -l | { cat; echo "@weekly $application_dir/get-torrents  > /var/log/get-torrents.log"; } | crontab -
 }
 
 function copy_preseeds ()

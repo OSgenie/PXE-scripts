28ed9cc914c6486438f87c7c467acab3cd44fa5f
kirtley <kirtley@wienbroer.com>
2019-01-02 20:09:31 -0500

    new file:   get-torrents.sh 	new file:   torrent.configs/kubuntu.config 	new file:   torrent.configs/linuxmint.config 	new file:   torrent.configs/lubuntu.config 	new file:   torrent.configs/ubuntu-budgie.config 	new file:   torrent.configs/ubuntu-mate.config 	new file:   torrent.configs/ubuntu.config 	new file:   torrent.configs/ubuntustudio.config 	new file:   torrent.configs/xubuntu.config

---
+++ get-torrents.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+distros=torrent.configs/*
+download_folder=/var/nfs/transmission/torrents/
+
+for distro in $distros; do
+  distro=$(basename -s .config $distro)
+  source torrent.configs/$distro".config"
+  for arch in $arches; do
+    for flavor in $flavors; do
+      for release in $releases; do
+        source $script_dir/$distro".config"
+        for i in {10..0}; do
+          cd $download_folder
+          if [ $i == 0 ];then
+            echo "checking "$release_server"/"$release_folder"/"$distro"-"$release"-"$flavor"-"$arch".iso.torrent"
+            /usr/bin/wget --no-clobber --show-progress -t 1 $release_server"/"$release_folder"/"$distro"-"$release"-"$flavor"-"$arch".iso.torrent"
+          else
+            echo "checking "$release_server"/"$release_folder"/"$distro"-"$release"."$i"-"$flavor"-"$arch".iso.torrent"
+            /usr/bin/wget --no-clobber --show-progress -t 1 $release_server"/"$release_folder"/"$distro"-"$release"."$i"-"$flavor"-"$arch".iso.torrent"
+          fi
+          if [ -f $distro"-"$release"."$i"-"$flavor"-"$arch".iso.torrent" ] || [ -f  $distro"-"$release"-"$flavor"-"$arch".iso.torrent" ] \
+          || [ -f $distro"-"$release"."$i"-"$flavor"-"$arch".iso.torrent.added" ] || [ -f  $distro"-"$release"-"$flavor"-"$arch".iso.torrent.added" ];then
+            break
+          fi
+        done
+      done
+    done
+  done
+done

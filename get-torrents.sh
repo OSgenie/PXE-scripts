45aa5ecef75b9e7d90d42bf336db50b7a837127f
kirtley <kirtley@wienbroer.com>
2019-01-02 21:54:43 -0500

    modified:   get-torrents.sh

--- get-torrents.sh
+++ get-torrents.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
-script_dir=/usr/local/bin/torrent.configs
-distros_base=torrent.configs/*
+distro_dir=/usr/local/bin/torrent.configs
 download_folder=/var/nfs/transmission/torrents/
 
-for distros in $distros_base; do
-  for distro in $distros; do
+for distros in $distro_dir/*; do
+  for distro in $distros/*; do
     distros=$(basename $distros)
     distro=$(basename -s .config $distro)
-    source $script_dir/$distros/$distro".config"
-    source $script_dir/$distros/$distros".arches"
+    source $distro_dir/$distros/$distro".config"
+    source $distro_dir"/."$distros"/arches"
     for arch in $arches; do
       for flavor in $flavors; do
-        source $script_dir/$distros/$distros".releases"
+        source $distro_dir"/."$distros"/releases"
         for release in $releases; do
           for i in {10..0}; do
-            source $script_dir/$distros/$distro".config"
+            source $distro_dir/$distros/$distro".config"
             if [ -f $distro"-"$release"."$i"-"$flavor"-"$arch".iso.torrent.added" ] || [ -f  $distro"-"$release"-"$flavor"-"$arch".iso.torrent.added" ];then
               break
             fi

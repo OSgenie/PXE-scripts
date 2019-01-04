1ac4449a497c991c2f73121d8c8b64032980f157
kirtley <kirtley@wienbroer.com>
2019-01-04 14:33:37 -0500

    modified:   get-torrents.sh 	modified:   torrent.configs/.linuxmint/releases 	new file:   torrent.configs/.ubuntu-18/arches 	new file:   torrent.configs/.ubuntu-18/releases 	new file:   torrent.configs/ubuntu-18/ubuntu.config 	modified:   torrent.configs/ubuntu/ubuntu.config

--- get-torrents.sh
+++ get-torrents.sh
@@ -20,10 +20,10 @@ for distros in $distro_dir/*; do
             cd $download_folder
             if [ $i == 0 ];then
               echo "checking "$release_server"/"$release_folder"/"$distro"-"$release"-"$flavor"-"$arch".iso.torrent"
-              /usr/bin/wget --no-clobber -t 1 -T 1 $release_server"/"$release_folder"/"$distro"-"$release"-"$flavor"-"$arch".iso.torrent"
+              /usr/bin/wget -t 1 -T 1 $release_server"/"$release_folder"/"$distro"-"$release"-"$flavor"-"$arch".iso.torrent"
             else
               echo "checking "$release_server"/"$release_folder"/"$distro"-"$release"."$i"-"$flavor"-"$arch".iso.torrent"
-              /usr/bin/wget --no-clobber -t 1 -T 1 $release_server"/"$release_folder"/"$distro"-"$release"."$i"-"$flavor"-"$arch".iso.torrent"
+              /usr/bin/wget -t 1 -T 1 $release_server"/"$release_folder"/"$distro"-"$release"."$i"-"$flavor"-"$arch".iso.torrent"
             fi
             if [ -f $distro"-"$release"."$i"-"$flavor"-"$arch".iso.torrent" ] || [ -f  $distro"-"$release"-"$flavor"-"$arch".iso.torrent" ];then
               break

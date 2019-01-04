5913eb98d7dc9daa373f1a7fc2596843a525d53d
kirtley <kirtley@wienbroer.com>
2019-01-04 17:45:05 -0500

    modified:   create-preseed-files.sh

--- create-preseed-files.sh
+++ create-preseed-files.sh
@@ -11,19 +11,19 @@ source preseed.configs/server-default.config
 
 cat > $http_preseed_root/uinstall << \
 EOF
+  # Installer Settings
   d-i	debian-installer/locale	string en_US.UTF-8
   d-i	debian-installer/splash boolean false
-
+  # Language Settings
   d-i	console-setup/ask_detect	boolean false
   d-i	console-setup/layoutcode	string us
   d-i	console-setup/variantcode	string
-
+  # Network Settings
   d-i	netcfg/get_nameservers	string $dns_name_servers
   d-i	netcfg/get_ipaddress	string $ip_address
   d-i	netcfg/get_netmask	string $network_netmask
   d-i	netcfg/get_gateway	string $network_gateway
   d-i	netcfg/confirm_static	boolean false
-
   # This automatically creates a standard unencrypted partitioning scheme.
   d-i partman-auto/disk string /dev/sda
   d-i partman-auto/method string regular
@@ -50,55 +50,52 @@ EOF
                           use_filesystem{ } filesystem{ ext4 } \\
                           mountpoint{ / } \\
                   .
-
   d-i partman-md/confirm boolean false
   d-i partman-partitioning/confirm_write_new_label boolean false
   d-i partman/choose_partition select finish
   d-i partman/confirm boolean false
   d-i partman/confirm_nooverwrite boolean false
-
+  # Time Settings
   d-i clock-setup/utc boolean true
   d-i time/zone string US/Eastern
   d-i	clock-setup/ntp	boolean true
   d-i	clock-setup/ntp-server	string ntp.ubuntu.com
-
+  # Base install
   d-i	base-installer/kernel/image	string linux-server
-
+  # User Account
   d-i	passwd/root-login	boolean false
   d-i	passwd/make-user	boolean true
   d-i	passwd/user-fullname	string ubuntu
   d-i	passwd/username	string ubuntu
-  d-i	passwd/user-password-crypted	password $6$.1eHH0iY$ArGzKX2YeQ3G6U.mlOO3A.NaL22Ewgz8Fi4qqz.Ns7EMKjEJRIW2Pm/TikDptZpuu7I92frytmk5YeL.9fRY4.
+  d-i	passwd/user-password-crypted	password \$6\$.1eHH0iY\$ArGzKX2YeQ3G6U.mlOO3A.NaL22Ewgz8Fi4qqz.Ns7EMKjEJRIW2Pm/TikDptZpuu7I92frytmk5YeL.9fRY4.
   d-i	passwd/user-uid	string
   d-i	user-setup/allow-password-weak	boolean false
   d-i	user-setup/encrypt-home	boolean false
   d-i	passwd/user-default-groups	string adm cdrom dialout lpadmin plugdev sambashare
-
+  # Apt Configuration
   d-i	apt-setup/services-select	multiselect security
   d-i	apt-setup/security_host	string security.ubuntu.com
   d-i	apt-setup/security_path	string /ubuntu
-
   d-i mirror/http/mirror select CC.archive.ubuntu.com
   d-i mirror/http/proxy string http://$server_ip:3142/
-
+  # Additional Packages
   d-i debian-installer/allow_unauthenticated	string false
   d-i	pkgsel/upgrade	select safe-upgrade
   d-i	pkgsel/language-packs	multiselect
   d-i	pkgsel/update-policy	select none
   d-i	pkgsel/updatedb	boolean true
-
+  # Grub Configuration
   d-i	grub-installer/skip	boolean false
   d-i	lilo-installer/skip	boolean false
   d-i	grub-installer/only_debian	boolean true
   d-i	grub-installer/with_other_os	boolean true
-
+  # Finish Options
   d-i	finish-install/keep-consoles	boolean false
   d-i	finish-install/reboot_in_progress	note
-
   d-i	cdrom-detect/eject	boolean true
   d-i	debian-installer/exit/halt	boolean false
   d-i	debian-installer/exit/poweroff	boolean false
-
+  # Post System Installation Tasks
   d-i	pkgsel/include string byobu vim openssh-server git-core landscape-common
   byobu	byobu/launch-by-default boolean true
 EOF
